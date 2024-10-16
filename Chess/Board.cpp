78d2ed9198c100d13aad2c402709aa1d3b0197af
moomie1 <rileyk2809@gmail.com>
2024-10-15 19:14:48 -0700

    made some board changes

---
+++ Chess/Board.cpp
@@ -0,0 +1,29 @@
+#include "Board.h"
+#include <unordered_map>
+#include <vector>
+
+
+
+void Piece::location(int x, int y) {
+
+}
+
+bool Piece::isAlive() {
+
+}
+
+void Board::writeBoard() {
+
+}
+
+void Board::updateBoard() {
+
+}
+
+void Board::pieceStatus() {
+
+}
+
+void Board::boardStatus()
+{
+}
--- Chess/Board.h
+++ Chess/Board.h
@@ -1,1 +1,32 @@
-#pragma once
+#ifndef BOARD_H
+#define BOARD_H
+
+#include <unordered_map>
+#include <array>
+#include <string>
+
+class Piece {
+public:
+	void location(int x, int y);
+	bool isAlive();
+	int id;
+
+private:
+	std::unordered_map<int, std::string> pieceType;
+	std::array<std::array<int, 8>, 8> position;
+};
+
+class Board {
+public:
+	void writeBoard();
+	void updateBoard();
+	void pieceStatus();
+	void boardStatus();
+
+private:
+	std::unordered_map<bool, int> aliveCheck;
+};
+
+
+
+#endif // BOARD_H
